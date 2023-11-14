fbe81cac8352ef7494ab894575fecfcbd0bdfb94
SATYAM TRIPATHI <tripathis24@outlook.com>
2023-11-14 11:19:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.cross').style.display ='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+
+});
+// console.log("hi");
